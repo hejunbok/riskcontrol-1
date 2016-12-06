3eef02ada7ee5ea62aab4f0cd97b90e7aab1ea64
shihuanzhao(赵诗环) <shihuanzhao@tencent.com>
2016-12-07 00:04:16 +0800

    minor: modify import path

--- code/prepare/parsebill.py
+++ code/prepare/parsebill.py
@@ -1,12 +1,14 @@
-from code.constants import path_fmt,col_names,table_set,bank_detail_func
+from code.constants import path_fmt,col_names,table_set
 import pandas as pd
 import numpy as np
+from os import path
 
 table = "bill_detail"
 datatype = 'train'
 
-path = path_fmt[table]%(datatype,datatype)
-df = pd.read_csv(path,header = None)
+
+input_path = path.join(path.dirname(__file__),path_fmt[table]%(datatype,datatype))
+df = pd.read_csv(input_path,header = None)
 df.columns = col_names[table]
 
 '''col = ["PayStatus"]'''
@@ -24,7 +26,8 @@ for uid,group in df_group:
                     MaxTransactionNum = group['TransactionNum'].max(),
                     AvgCurrentBill = group['CurrentBill'].mean(),
                     )
-    print 'need selecting more features'
+
     res_dic[uid] = selectFeature
+print 'need selecting more features'
 res_df = pd.DataFrame(res_dic)
 res_df.to_csv('../../tmp/bill_detail_clean.csv')
