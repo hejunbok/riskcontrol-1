595420a5a70e783425cf6aa66e6f5c8ca3e7310d
shihuanzhao(赵诗环) <shihuanzhao@tencent.com>
2016-12-07 00:10:26 +0800

    minor: change datatype from dict 2 OrderedDict

--- code/prepare/parsebill.py
+++ code/prepare/parsebill.py
@@ -2,22 +2,22 @@ from code.constants import path_fmt,col_names,table_set
 import pandas as pd
 import numpy as np
 from os import path
+from collections import OrderedDict
+prepath = path.dirname(__file__)
 
 table = "bill_detail"
 datatype = 'train'
 
-prepath = path.dirname(__file__)
 input_path = path.join(prepath,path_fmt[table]%(datatype,datatype))
 output_path = path.join(prepath,'../../tmp/bill_detail_clean.csv')
 
 df = pd.read_csv(input_path,header = None)
 df.columns = col_names[table]
 
-'''col = ["PayStatus"]'''
 df_group = df[df.PayStatus == 0].groupby(by = 'uid')
 res_dic = dict()
 for uid,group in df_group:
-    selectFeature = dict(
+    selectFeature = OrderedDict(
                     billNum=group.shape[0],
                     bankNum = len(np.unique(group['bankid'])),
                     AvgLastBill = group['LastBill'].mean(),
@@ -28,7 +28,6 @@ for uid,group in df_group:
                     MaxTransactionNum = group['TransactionNum'].max(),
                     AvgCurrentBill = group['CurrentBill'].mean(),
                     )
-
     res_dic[uid] = selectFeature
 print 'need selecting more features'
 res_df = pd.DataFrame(res_dic)
