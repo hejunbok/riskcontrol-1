37b34f4a2ed5d901da6b87575ccedb2f2d616524
shihuanzhao(赵诗环) <shihuanzhao@tencent.com>
2016-12-06 14:19:19 +0800

    minor: parse X,Y

--- code/constants.py
+++ code/constants.py
@@ -1,9 +1,20 @@
 
-Y_path_fmt = "../../data/%s/overdue_%s.txt"
-X_path_fmt = dict(
+
+table_set = ["loan_time","browse_history","bank_detail","bill_detail","user_info","overdue"]
+path_fmt = dict(
             loan_time = "../../data/%s/loan_time_%s.txt",
             browse_history = "../../data/%s/browse_history_%s.txt",
             bank_detail = "../../data/%s/bank_detail_%s.txt",
             bill_detail = "../../data/%s/bill_detail_%s.txt",
             user_info= "../../data/%s/user_info_%s.txt",
+            overdue = "../../data/%s/overdue_%s.txt"
             )
+
+col_names = dict(
+                overdue = ['uid','overdue'],
+                loan_time = ['uid','timestamp'] ,
+                browse_history = ["uid","timestamp","pv","BrowseType"],
+                bank_detail = ["uid","timestamp","iTradeType","iCost","IncomeFlag"],
+                bill_detail =["uid","timestamp","bankid","LastBill","LastPayment","creditAmount","CurrentBalance","CurrentleastPayment","TransactionNum","CurrentBill","AdjustAmt","CycleInterest","AvalibeLAmt","PreBorrowAmt" ,"paystatus"] ,
+                user_info = ["uid","sex","career","education","MaritalStatus","ResidenceType"],
+                )
--- code/prepare/parsingtxt.py
+++ code/prepare/parsingtxt.py
@@ -1,50 +1,20 @@
-from code.constants import Y_path_fmt,X_path_fmt
+from code.constants import path_fmt,col_names,table_set
 import pandas as pd
 
 datatype = 'train'
-
-df = []
-
-'''X'''
-''' 1.user_info'''
-X_path = X_path_fmt['user_info']%(datatype,datatype)
-user_info_df = pd.read_csv(X_path,header = None)
-user_info_df.columns = ["uid","sex","career","education","MaritalStatus","ResidenceType"]
-df.append(user_info_df)
-
-''' 2. bank_detail'''
-X_path = X_path_fmt['bank_detail']%(datatype,datatype)
-bank_detail_df = pd.read_csv(X_path,header = None)
-bank_detail_df.columns = ["uid","timestamp","iTradeType","iCost","IncomeFlag"]
-df.append(bank_detail_df)
-''' 3. browse_history'''
-X_path = X_path_fmt['browse_history']%(datatype,datatype)
-browse_history_df = pd.read_csv(X_path,header = None)
-browse_history_df.columns = ["uid","timestamp","pv","BrowseType"]
-df.append(browse_history_df)
-
-''' 4. bill_detail'''
-X_path = X_path_fmt['bill_detail']%(datatype,datatype)
-bill_detail_df = pd.read_csv(X_path,header = None)
-bill_detail_df.columns = ["uid","timestamp","bankid","LastBill","LastPayment","creditAmount","CurrentBalance","CurrentleastPayment","TransactionNum","CurrentBill","AdjustAmt","CycleInterest","AvalibeLAmt","PreBorrowAmt" ,"paystatus"]
-df.append(bill_detail_df)
-
-'''5.loan_time'''
-X_path = X_path_fmt['loan_time']%(datatype,datatype)
-loan_time_df = pd.read_csv(X_path,header = None)
-loan_time_df.columns = ['uid','timestamp']
-df.append(loan_time_df)
-
-'''6. Y'''
-Y_path = Y_path_fmt%(datatype,datatype)
-overdue_df = pd.read_csv(Y_path,header = None)
-overdue_df.columns = ['uid','overdue']
-df.append(overdue_df)
+df_set = dict()
+'''table_set = ["loan_time","browse_history","bank_detail","bill_detail","user_info","overdue"]'''
+for table in table_set:
+    path = path_fmt[table]%(datatype,datatype)
+    df = pd.read_csv(path,header = None)
+    df.columns = col_names[table]
+    df_set[table] = df
+    print table
 
 '''merge X and Y into table'''
 
-res_df = pd.merge(df[-1],df[-2],how = 'left',on = 'uid')
-res_df = pd.merge(res_df,df[0],how = 'left',on = 'uid')
+res_df = pd.merge(df_set['overdue'],df_set['loan_time'],how = 'left',on = 'uid')
+res_df = pd.merge(res_df,df_set['user_info'],how = 'left',on = 'uid')
 
 print res_df.head()
 res_df.to_csv('../../tmp/XY.csv')
